9b2cf84949bf27e011a16c54a30af3f77e85a7cf
Tom Power <tpower21@gmail.com>
2021-10-22 17:37:53 +0100

    tidy

--- src/my/cyclefocus/config.lua
+++ src/my/cyclefocus/config.lua
@@ -6,14 +6,4 @@ cyclefocus.move_mouse_pointer = false
 cyclefocus.display_next_count = 10
 cyclefocus.display_prev_count = 10
 
-cyclefocus.cycle_filters = {
-    function(c, source_c) return c end
-}
-cyclefocus.cycleAllTags = {
-  cycle_filters = { cyclefocus.filters.same_screen }
-}
-cyclefocus.cycleSameTag = {
-  cycle_filters = { cyclefocus.filters.same_screen, cyclefocus.filters.common_tag }
-}
-
 cyclefocus.preset_for_offset = require('my.cyclefocus.presetForOffset')
--- src/my/keys/cycleFocus.lua
+++ src/my/keys/cycleFocus.lua
@@ -2,6 +2,16 @@ local cyclefocus = require('cyclefocus')
 local gears = require("gears")
 require("my.cyclefocus.config")
 
+cyclefocus.cycle_filters = {
+    function(c, source_c) return c end
+}
+cyclefocus.cycleAllTags = {
+  cycle_filters = { cyclefocus.filters.same_screen }
+}
+cyclefocus.cycleSameTag = {
+  cycle_filters = { cyclefocus.filters.same_screen, cyclefocus.filters.common_tag }
+}
+
 return gears.table.join(
   cyclefocus.key({ ctrl }, tab, cyclefocus.cycleAllTags),
   cyclefocus.key({ alt }, tab, cyclefocus.cycleSameTag)
