144871f2a0fe43368a0823e635c1afe1fd7a15ba
Tom Power <tpower21@gmail.com>
2021-10-29 08:48:40 +0100

    tidy

--- src/my/helper/moveClient.lua
+++ src/my/helper/moveClient.lua
@@ -5,20 +5,20 @@ local function currentTag()
   return client.focus and client.focus.first_tag or nil
 end
 
-local function tagWithOffset(tag, offset)
+local function tagAtIndex(index)
   local tags = client.focus.screen.tags
-  return tags[gmath.cycle(#tags, tag.name + offset)]
+  return tags[gmath.cycle(#tags, index)]
 end
 
 local moveClient = {}
 
 function moveClient.toPrevTag()
-    awful.client.movetotag(tagWithOffset(currentTag(), -1))
+    awful.client.movetotag(tagAtIndex(currentTag().name - 1))
     awful.tag.viewprev()
 end
 
 function moveClient.toNextTag()
-    awful.client.movetotag(tagWithOffset(currentTag(), 1))
+    awful.client.movetotag(tagAtIndex(currentTag().name + 1))
     awful.tag.viewnext()
 end
 
