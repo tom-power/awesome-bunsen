d79ba63f66ebd9fb1353dc9df3b4d2d701445ef1
Tom Power <tpower21@gmail.com>
2021-11-09 19:09:02 +0000

    formating

--- src/rc.lua
+++ src/rc.lua
@@ -84,7 +84,8 @@ clientkeys = gears.table.join(
 awful.rules.rules = {
   -- All clients will match this rule.
   { rule = { },
-    properties = { border_width = 1,
+    properties = {
+      border_width = 1,
       border_color = beautiful.border_normal,
       focus = awful.client.focus.filter,
       raise = true,
